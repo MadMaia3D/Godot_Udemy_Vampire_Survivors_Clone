29851c064bdb2bbf71afe8a5fdb6b56c87bd4501
MadMaia3D <33674747+MadMaia3D@users.noreply.github.com>
2024-08-19 11:47:25 -0300

    feat: enemy manager to spawn enemies

---
+++ scenes/manager/enemy_manager.gd
@@ -0,0 +1,23 @@
+extends Node
+
+var spawn_radius = 250
+
+@export var basic_enemy_scene: PackedScene
+
+
+func _ready():
+	$Timer.timeout.connect(on_timer_timeout)
+
+
+func on_timer_timeout():
+	var player: Node2D = get_tree().get_first_node_in_group("player") as Node2D
+	if player == null:
+		return
+	
+	var random_angle: float = randf_range(-PI, PI)
+	var random_direction: Vector2 = Vector2.RIGHT.rotated(random_angle)
+	var spawn_position: Vector2 = player.global_position + (random_direction * spawn_radius)
+	
+	var enemy_instance: Node2D = basic_enemy_scene.instantiate() as Node2D
+	get_parent().add_child(enemy_instance)
+	enemy_instance.global_position = spawn_position
