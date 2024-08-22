1f3b0811888f3dd7b67435e3838825ea24b451f8
MadMaia3D <33674747+MadMaia3D@users.noreply.github.com>
2024-08-22 12:09:40 -0300

    refactor: VelocityComponent to encapsulate velocity logic

---
+++ scenes/component/velocity_component.gd
@@ -0,0 +1,30 @@
+class_name VelocityComponent
+extends Node
+
+@export var max_speed: float = 40
+@export var acceleration: float = 5
+
+var velocity: Vector2 = Vector2.ZERO
+
+
+func accelerate_to(target: Node2D) -> void:
+	var ownner_node2d = owner as Node2D
+	var direction: Vector2 = ownner_node2d.global_position.direction_to(target.global_position)
+	accelerate_in_direction(direction)
+
+
+func accelerate_to_player() -> void:
+	var player_node = get_tree().get_first_node_in_group("player") as Node2D
+	if player_node:
+		accelerate_to(player_node)
+
+
+func accelerate_in_direction(direction: Vector2) -> void:
+	var desired_velocity = direction * max_speed
+	velocity = velocity.lerp(desired_velocity, 1 - exp(-acceleration * get_process_delta_time()))
+
+
+func move(character_body: CharacterBody2D) -> void:
+	character_body.velocity = velocity
+	character_body.move_and_slide()
+	velocity = character_body.velocity
--- scenes/game_object/basic_enemy/basic_enemy.gd
+++ scenes/game_object/basic_enemy/basic_enemy.gd
@@ -1,26 +1,17 @@
 extends CharacterBody2D
 
-const MAX_SPEED : float = 30.0
 
-@onready var health_component: HealthComponent = $HealthComponent
 @onready var visual = $Visual
+@onready var velocity_component: VelocityComponent = $VelocityComponent
 
 
 func _process(delta):
-	var direction = get_direction_to_player()
-	velocity = direction * MAX_SPEED
-	move_and_slide()
-	update_animation(direction)
+	velocity_component.accelerate_to_player()
+	velocity_component.move(self)
+	update_animation()
 
 
-func update_animation(direction) -> void:
-	var flip = sign(direction.x)
-	if direction.x != 0.0:
+func update_animation() -> void:
+	var flip = sign(velocity.x)
+	if velocity.x != 0.0:
 		visual.scale.x = flip
-
-
-func get_direction_to_player() -> Vector2:
-	var player_node = get_tree().get_first_node_in_group("player") as Node2D
-	if player_node:
-		return global_position.direction_to(player_node.global_position)
-	return Vector2.ZERO
