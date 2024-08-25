5086fedf2a31b88848ece9e4da375e8f4662dcc4
MadMaia3D <33674747+MadMaia3D@users.noreply.github.com>
2024-08-24 23:18:57 -0300

    feat: victory and defeat jingles

--- scenes/UI/end_screen.gd
+++ scenes/UI/end_screen.gd
@@ -19,6 +19,14 @@ func set_defeat():
 	%DescriptionLabel.text = "We are sorry!"
 
 
+func play_victory_jingle() -> void:
+	$VictoryAudioStreamPlayer.play()
+
+
+func play_defeat_jingle() -> void:
+	$DefeatAudioStreamPlayer.play()
+
+
 func on_restart_button_pressed() -> void:
 	get_tree().paused = false
 	get_tree().change_scene_to_file("res://scenes/main/main.tscn")
