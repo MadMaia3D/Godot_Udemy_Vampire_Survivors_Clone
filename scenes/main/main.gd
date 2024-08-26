9ed55e2e1facd9293025903db68fdb399cc6c83a
MadMaia3D <33674747+MadMaia3D@users.noreply.github.com>
2024-08-26 16:44:17 -0300

    fix: animations bugs

--- scenes/main/main.gd
+++ scenes/main/main.gd
@@ -11,7 +11,6 @@ func _ready():
 
 func _unhandled_input(event):
 	if event.is_action_pressed("pause"):
-		print("trash")
 		add_child(pause_menu_scene.instantiate())
 	get_tree().root.set_input_as_handled()
 
