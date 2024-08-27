dd238b1626d362d17dbf6083b4ff15fdb79619c8
MadMaia3D <33674747+MadMaia3D@users.noreply.github.com>
2024-08-27 20:29:26 -0300

    feat: update pause menu

--- scenes/UI/end_screen.gd
+++ scenes/UI/end_screen.gd
@@ -3,8 +3,8 @@ extends CanvasLayer
 
 func _ready():
 	GameEvents.pause_game()
-	%RestartButton.pressed.connect(on_restart_button_pressed)
-	%QuitButton.pressed.connect(on_quit_button_pressed)
+	%ContinueButton.pressed.connect(on_continue_button_pressed)
+	%MainMenuButton.pressed.connect(on_main_menu_button_pressed)
 
 	var panel_container:PanelContainer = %PanelContainer
 	panel_container.pivot_offset = panel_container.size/2
@@ -27,11 +27,11 @@ func play_defeat_jingle() -> void:
 	$DefeatAudioStreamPlayer.play()
 
 
-func on_restart_button_pressed() -> void:
+func on_continue_button_pressed() -> void:
 	MetaProgression.save_game()
-	ScreenTransition.transition_to_scene_and_unpause("res://scenes/main/main.tscn")
+	ScreenTransition.transition_to_scene_and_unpause("res://scenes/UI/meta_menu.tscn")
 
 
-func on_quit_button_pressed() -> void:
+func on_main_menu_button_pressed() -> void:
 	MetaProgression.save_game()
 	ScreenTransition.transition_to_scene_and_unpause("res://scenes/UI/main_menu.tscn")
