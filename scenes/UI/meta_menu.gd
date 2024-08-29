1c3da1af27a7b6d8c8c7aa6dca05885a5986f4a6
MadMaia3D <33674747+MadMaia3D@users.noreply.github.com>
2024-08-28 21:31:50 -0300

    feat: add a scroll bar container to the meta menu

--- scenes/UI/meta_menu.gd
+++ scenes/UI/meta_menu.gd
@@ -3,8 +3,8 @@ extends CanvasLayer
 @export var meta_upgrades_list: Array[MetaUpgrade] = []
 
 @onready var meta_card_scene = preload("res://scenes/UI/meta_upgrade_card.tscn")
-@onready var grid_container = $MarginContainer/GridContainer
-@onready var back_button = $MarginContainer/BackButton
+@onready var grid_container = %GridContainer
+@onready var back_button = %BackButton
 
 
 func _ready():
@@ -12,7 +12,7 @@ func _ready():
 	
 	for placeholder in grid_container.get_children():
 		placeholder.queue_free()
-	
+
 	for upgrade in meta_upgrades_list:
 		var meta_card_instance: MetaUpgradeCard = meta_card_scene.instantiate() as MetaUpgradeCard
 		grid_container.add_child(meta_card_instance)
